5ad6fc029e200e2539c4ead9afd643c31c7ab75c
Bertil <bertilvb@xs4all.nl>
2017-09-13 12:33:35 +0200

    uitgewerkte opdracht

--- Le03Verkiezingen/Herhalingsprogramma.java
+++ Le03Verkiezingen/Herhalingsprogramma.java
@@ -5,6 +5,17 @@ import verkiezingen.Kandidaat;
 
 public class Herhalingsprogramma {
   public static void main(String[] args) {
+	  // opdracht: Schrijf een programma dat alle partijen ne haar kandidaten print.
+	  Stemmachine machine = new Stemmachine();
+	  machine.zetAan();
+	  for (Partij p: machine.getPartijen()){
+		  System.out.println(p.getNaam());
+		  int kandidaatNummer = 1;
+		  for (Kandidaat k: p.getKandidaten()){
+			  System.out.println("  " + kandidaatNummer + ". " + k.getNaam() + " uit " + k.getWoonplaats());
+			  kandidaatNummer = kandidaatNummer + 1;
+		  }
+		  System.out.println("");		  
+	  }
   }
 }
-
